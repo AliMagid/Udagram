47a52b894bb8af829e58eb77439ea733bc8e66d1
Fego <e.mohammedfarag@gmail.com>
2022-09-24 23:16:40 +0200

    configure k8s files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a8d5501132fb74e5db0de83c58758e9a-437228782.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
